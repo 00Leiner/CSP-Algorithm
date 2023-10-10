cb94ff1ac6e808887d872b2bbc5e20c0e07b6ee1
Leiner <leinersacdalan4@gmail.com>
2023-10-10 13:45:15 +0800

    sample

--- python/app.py
+++ python/app.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+from constraint import *
+
+problem = Problem()
+
+# Define variables and domains
+students = ['Y_s1', 'Y_s2', 'Y_s3', 'Y_s4', 'Y_s5']
+for student in students:
+    problem.addVariable(student, [0, 1])
+
+# Define constraints
+def student_constraint(*args):
+    return sum(args) <= 5
+
+problem.addConstraint(student_constraint, students)
+
+solutions = problem.getSolutions()
+print(solutions)
